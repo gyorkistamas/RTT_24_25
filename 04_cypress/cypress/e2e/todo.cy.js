067428bb29eff2d67eb1627a646d2c1bde0d162c
Györkis Tamás <tamasgyorkis@gmail.com>
2024-10-08 17:04:22 +0200

    added example tests

--- 04_cypress/cypress/e2e/todo.cy.js
+++ 04_cypress/cypress/e2e/todo.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Testing To-do app', () => {
+  beforeEach(() =>{
+    cy.visit("https://example.cypress.io/todo")
+  });
+
+  it('displays two item in the list', () => {
+    cy.get('.todo-list li').should('have.length', 2);
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill');
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog');
   })
+
+  it('can add a new element to the list', () => {
+    const newItem = 'Feed the dog';
+
+    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`);
+
+    cy.get('.todo-list li')
+    .should('have.length', 3)
+    .last()
+    .should('have.text', newItem);
+
+  });
+
 })
